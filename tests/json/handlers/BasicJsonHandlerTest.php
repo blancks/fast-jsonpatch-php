94875030533319914705f3dd70c996e21480752c
Salvatore <s.rotondo90@gmail.com>
2024-12-18 22:51:00 +0100

    Test cases for new methods in BasicJsonHandler class

--- tests/json/handlers/BasicJsonHandlerTest.php
+++ tests/json/handlers/BasicJsonHandlerTest.php
@@ -6,21 +6,33 @@ use blancks\JsonPatch\exceptions\MalformedDocumentException;
 use blancks\JsonPatch\json\accessors\ArrayAccessor;
 use blancks\JsonPatch\json\accessors\ObjectAccessor;
 use blancks\JsonPatch\json\handlers\BasicJsonHandler;
+use blancks\JsonPatch\json\pointer\JsonPointer6901;
+use blancks\JsonPatch\json\pointer\JsonPointerHandlerInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(BasicJsonHandler::class)]
 #[UsesClass(MalformedDocumentException::class)]
+#[UsesClass(JsonPointer6901::class)]
 #[UsesClass(ArrayAccessor::class)]
 #[UsesClass(ObjectAccessor::class)]
 class BasicJsonHandlerTest extends TestCase
 {
     private BasicJsonHandler $BasicJsonHandler;
 
+    /** @var JsonPointerHandlerInterface&MockObject */
+    private JsonPointerHandlerInterface $JsonPointerHandlerMock;
+
+    /**
+     * @return void
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
     protected function setUp(): void
     {
-        $this->BasicJsonHandler = new BasicJsonHandler();
+        $this->JsonPointerHandlerMock = $this->createMock(JsonPointerHandlerInterface::class);
+        $this->BasicJsonHandler = new BasicJsonHandler(JsonPointerHandler: $this->JsonPointerHandlerMock);
         parent::setUp();
     }
 
@@ -72,5 +84,31 @@ class BasicJsonHandlerTest extends TestCase
         $json = "\xb11";
         $this->expectException(MalformedDocumentException::class);
         $this->BasicJsonHandler->decode($json);
+    }
+
+    /**
+     * Tests that the result of the isValidPointer method is determined by
+     * the JsonPointerHandler passed as class dependency
+     * @return void
+     */
+    public function testIsValidPointer(): void
+    {
+        $this->JsonPointerHandlerMock->method('isValidPointer')->willReturn(true);
+        $this->assertTrue($this->BasicJsonHandler->isValidPointer('...'));
+
+        $this->JsonPointerHandlerMock->method('isValidPointer')->willReturn(false);
+        $this->assertTrue($this->BasicJsonHandler->isValidPointer('...'));
+    }
+
+    /**
+     * Tests that the result of the getTokensFromPointer method is determined by
+     * the JsonPointerHandler passed as class dependency
+     * @return void
+     */
+    public function testGetTokensFromPointer(): void
+    {
+        $expected = ['some','path'];
+        $this->JsonPointerHandlerMock->method('getTokensFromPointer')->willReturn($expected);
+        $this->assertSame($expected, $this->BasicJsonHandler->getTokensFromPointer('...'));
     }
 }
